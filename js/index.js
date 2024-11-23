d2e6326ecb2c37e3b4d8e6c32a93df59580054e3
pawan932 <142396193+pawan932@users.noreply.github.com>
2024-11-23 13:47:44 +0530

    font issue solved

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-let product = {
-    name : "highComb",
-    price : 99,
-    discunt : 0.10,
-    finalPrice : 99 - (99 * 0.10),
-};  
-console.log(product.final)
+let a = 5;
+let b = 9;
+
+console.log("b ** a =" , b ** a);
