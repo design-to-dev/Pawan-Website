0be3d1fd304e5e1e1b5b878512075079ce405473
pawan932 <142396193+pawan932@users.noreply.github.com>
2024-11-24 18:43:51 +0530

    apply operators in javascript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
-let a = 5;
-let b = 9;
+// Comparitive operators in javascript
 
-console.log("b ** a =" , b ** a);
+// let nikhil = 0;
+// let ashish = 80000;
+// let vp = 22500;
+
+// // Here vp wants grater value then her so she could continue her life with him
+// console.log("is nikhil deserves vp" , nikhil > vp);
+// console.log("is ashish deserves vp" , ashish > vp);
+
+// Logocal operators in javascript
+
+// let a = 5;
+// let b = 3;
+// let c = 3;
+
+// let cond1 = a > b;
+// let cond2 = b === c;
+
+// console.log(cond1 && cond2)
+
+// if/else condition in javascript
+
+// const age = 15;
+
+// if (age >= 18) {
+//     console.log("chodi ji ko vote de sakta he");
+// }
+// else { console.log("sorry bro mummy papa ko bol jaldi karna tha");
+// }
+
+// Lets Solve some faltu questions for practice
+
+
