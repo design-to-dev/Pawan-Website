0a89fbe61fe969f6c32c3f2c3149ae33a2cf1387
pawan932 <142396193+pawan932@users.noreply.github.com>
2024-11-24 22:47:25 +0530

    learned if/else condition in javascript

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@
 
 // if/else condition in javascript
 
-// const age = 15;
+// let age = 15;
 
 // if (age >= 18) {
 //     console.log("chodi ji ko vote de sakta he");
@@ -29,6 +29,32 @@
 // else { console.log("sorry bro mummy papa ko bol jaldi karna tha");
 // }
 
-// Lets Solve some faltu questions for practice
+// Lets Solve some faltu questions for practice of if/else
+
+// Lets Create Grade system 
+let score = prompt("Chal Apne marks daal");
+
+const gradea = ("Hey You recive grade A");
+const gradeb = ("Hey You recive grade b");
+const gradec = ("Hey You recive grade c");
+const graded = ("Hey You recive grade d");
+const fail = ("atty gatty🖕")
+
+if (score > 81 && score < 100){
+    console.log(gradea);
+}
+else if (score > 71 && score < 80){
+    console.log(gradeb);
+}
+ else if (score > 61 && score < 70){
+    console.log(gradec);
+}
+else if (score > 35 && score < 60){
+    console.log(graded);
+}
+else{
+    console.log(fail);
+}
+
 
 
